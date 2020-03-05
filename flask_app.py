f2660128de3477b8fffabd2b746d2aeb4fc0987e
Maira Martins Esteves <1902150@SALAS.AULAS>
2020-03-04 21:37:49 -0300

    servindo arquivos html

--- flask_app.py
+++ flask_app.py
@@ -1,11 +1,15 @@
 
 # A very simple Flask Hello World app for you to get started with...
 
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return '<h1>Meu primeiro site no Flask<h1>!'
+    return '<h1>Meu primeiro site no Flask localmente <h1>!'
+
+@app.route('/inicio')
+def inicio():
+    return render_template("index.html")
 
